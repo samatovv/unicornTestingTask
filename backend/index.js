9c25d407092fdb7d9b974028089508013f4139fc
Abdurahman <abdurahmansamatovic@gmail.com>
2025-05-22 16:08:42 +0600

    fix: port

--- backend/index.js
+++ backend/index.js
@@ -83,4 +83,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
-app.listen(3000, () => console.log('Сервер на http://localhost:3000'));
+app.listen(3000, '0.0.0.0', () => console.log('Сервер на http://0.0.0.0:3000'));
+
